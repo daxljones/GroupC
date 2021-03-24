897fd2f9818f916ab19e0a6fdceffec386d41dd5
dax.jones@okstate.edu <daxljones@outlook.com>
2021-03-23 20:51:42 -0500

    Got socket connection working

---
+++ ServerStart.c
@@ -0,0 +1,8 @@
+#include "functions.h"
+#include "structs.h"
+
+int main()
+{
+    //printf("runs a manager/assistant test. User can input information to be searched for\ncurrently no results will be found but this demonstrates communication between manager and assistant\n\n");
+    server();
+}
